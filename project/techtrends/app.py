3f7f7c0cbc7f13b56a09d36c5140158bc3a0fcca
Pijnacker, Ronald <ronald.pijnacker@philips.com>
2022-10-12 08:58:26 +0200

    Add /healthz endpoint

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -65,6 +65,15 @@ def create():
 
     return render_template('create.html')
 
+@app.route('/healthz')
+def heathz():
+    response = app.response_class(
+        response = json.dumps({'result': 'OK - healthy'}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port='3111')
